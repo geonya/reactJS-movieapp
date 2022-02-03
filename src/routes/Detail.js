fca8e102cdb8e4ecba5d2fa8abf99c66bee53f9f
geonya <geony8410@gmail.com>
2022-02-03 16:36:52 +0900

    gh-pages

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 function Detail() {
-	const [movies, setMovies] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [movie, setMovie] = useState([]);
 	const { id } = useParams();
 	const getMovie = async () => {
 		const json = await (
@@ -9,11 +10,33 @@ function Detail() {
 				`https://yts.mx/api/v2/movie_details/json?movie_id=${id}`
 			)
 		).json();
+		setMovie(json.data.movie);
+		console.log(json.data.movie);
+		setLoading(false);
 	};
 	useEffect(() => {
 		getMovie();
 	}, []);
-	return <h1>Detail</h1>;
+	return (
+		<div>
+			{loading ? (
+				<h1>Loading...</h1>
+			) : (
+				<div>
+					<img src={movie.background_image} alt=""></img>
+					<h1>{movie.title_long}</h1>
+					<span>{movie.rating}</span>
+					<span>{movie.year}</span>
+					<p>{movie.summary}</p>
+					<ul>
+						{movie.genres.map((genre, index) => (
+							<li key={index}>{genre}</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Detail;
